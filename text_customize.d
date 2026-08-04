964484846d42d964401103f7f5a66e2e1d4fc126
Aravinda VK <mail@aravindavk.in>
2026-08-04 22:06:00 +0530

    Customize text
    Signed-off-by: Aravinda VK <mail@aravindavk.in>

---
+++ text_customize.d
@@ -0,0 +1,26 @@
+/+ dub.sdl:
+ dependency "chitra" path="../chitra-d"
+ +/
+
+import std.stdio;
+import chitra;
+
+void main()
+{
+    auto ctx = new Chitra(280, 130);
+    with (ctx)
+    {
+        background(255);  // White background
+        noStroke;
+        
+        font("American Typewriter", 20);
+        // Same as above
+        // font("American Typewriter");
+        // fontSize(20);
+        fill("blue");
+        text("Hello World!", 50, 50);
+
+        saveAs("output/customize-text.png", resolution: 72);
+    }
+}
+
